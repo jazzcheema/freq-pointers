ce471f23d513791b90565e913f97de232d0ff8c1
Jazz Cheema <jaskamalc@gmail.com>
2024-02-09 17:21:28 -0800

    finished two array obj, began working on same freq

--- same-frequency.js
+++ same-frequency.js
@@ -1,3 +1,15 @@
-// add whatever parameters you deem necessary & write docstring
-function sameFrequency() {
+"use strict";
+
+
+/** compares two inputs and determines if they have the same freq. */
+
+//can't sort
+
+function sameFrequency(nums1, nums2) {
+
+  let numFreq = {};
+
+
+
+
 }
--- two-array-object.js
+++ two-array-object.js
@@ -1,3 +1,22 @@
-// add whatever parameters you deem necessary & write docstring
-function twoArrayObject() {
+"use strict";
+
+
+/** returns an object with merged with arrays--> keys from
+ * array 1, and values from array 2
+ *  */
+
+//input: two arrays
+//output: object
+
+//loop over keys array, mergedobject[key] = values[i] || or null
+//return mergedObject
+function twoArrayObject(keysArray, valuesArray) {
+
+  let mergedObject = {};
+
+  for (let i = 0; i < keysArray.length; i++) {
+    mergedObject[keysArray[i]] = valuesArray[i] || null;
+  }
+
+  return mergedObject;
 }
