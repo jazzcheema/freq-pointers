18b02d94bbe265b27a880730b9e5860cb21262e5
Jazz Cheema <jaskamalc@gmail.com>
2024-02-10 17:52:23 -0800

    updated and completed 2 exercises

--- same-frequency.js
+++ same-frequency.js
@@ -2,14 +2,54 @@
 
 
 /** compares two inputs and determines if they have the same freq. */
-
-//can't sort
+/** our controller function. it takes in two numbers--> uses a method to split
+ * nums to strings in array--> moves one of those variables into freq. counter
+ * and checks the object to see if it's a match--> returns true if identical
+ * numerical inputs
+ */
 
 function sameFrequency(nums1, nums2) {
 
-  let numFreq = {};
+  let newNums1 = stringAndSplit(nums1);
+  let newNums2 = stringAndSplit(nums2);
+
+  newNums1 = makeObject(newNums1);
+
+  for (let num of newNums2) {
+    if (newNums1[num] !== undefined && newNums1[num] > 0) {
+      newNums1[num]--;
+    } else {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+/** makes object and frequency counter for nums--> once theyre converted
+ * to strings in an array
+ */
+
+function makeObject(nums) {
+
+  let newObject = {};
+
+  for (let num of nums) {
+    newObject[num] = (newObject[num] || 0) + 1;
+  }
+
+  return newObject;
+}
 
 
+/** takes a number, converts to strings, splits into array
+ * --> returns in string form */
 
+function stringAndSplit(num) {
 
+  let stringFromNum = JSON.stringify(num);
+
+  stringFromNum = stringFromNum.split('');
+
+  return stringFromNum;
 }
--- separate-positive.js
+++ separate-positive.js
@@ -1,3 +1,25 @@
-// add whatever parameters you deem necessary & write docstring
-function separatePositive() {
+"use strict";
+
+/**
+ * finds positive numbers and moves them to our tracked position that holds
+ * positive index. returns and array with positive to left and negative on right
+ */
+
+//test case: [5, 10, -15, 20, 25]
+// 5[0] 5[0]      10[1] 10[1]         20[3]  -15[2]   25[4]  -15[3]
+// 5[0] 5[0]      10[1] 10[1]         20[2]  -15[3]   25[3]  -15[4]
+// posSpace: 1       2                3                 4
+
+
+function separatePositive(nums) {
+
+  let positiveSpace = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] >= 0) {
+      [nums[i], nums[positiveSpace]] = [nums[positiveSpace], nums[i]];
+      positiveSpace++;
+    }
+  }
+  return nums;
 }
